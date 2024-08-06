53711713717b1fd1c01b019722c55eaccded2ab6
maonyanta <mao.810.hatto@gmail.com>
2024-08-06 22:40:40 +0200

    68. add question and answer button UI template

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -11,7 +11,32 @@ class QuestionsScreen extends StatefulWidget {
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
-  Widget build (context) {
-    return const Text('Questions Screen');
+  Widget build(context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('The question...'),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 1'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 2'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 3'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 4'),
+          ),
+        ],
+      ),
+    );
   }
 }
